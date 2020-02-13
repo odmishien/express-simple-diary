e9bdadcb3f3f66c67b3ba483040608ba126497a0
odmishien <tmtm.yagi.007@gmail.com>
2020-02-14 00:01:33 +0900

    オンメモリでentryを保持する

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let entries = [];
+
 app.use(express.static("views"));
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
 // router
-app.get("/", (req, res) => res.sendFile("index.html"));
-app.post("/", (req, res) => {
-  let message = req.body.message;
-  console.log(message);
-  res.send(message);
+app.get("/", (req, res) => res.render("index", { entries: entries }));
+app.post("/entry", (req, res) => {
+  let title = req.body.title;
+  let content = req.body.content;
+  let createdAt = new Date();
+  let entry = {
+    title: title,
+    content: content,
+    createdAt: createdAt,
+  };
+  entries.push(entry);
+  res.render("index", { entries: entries });
 });
 
 // server listen on localhost:3000
