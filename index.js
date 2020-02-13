fca514b4c1ff2dbd37c37ede6a2d47115745611e
odmishien <tmtm.yagi.007@gmail.com>
2020-02-13 21:20:01 +0900

    post受け取るかたち

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.use(express.static("views"));
+app.use(express.urlencoded({ extended: true }));
 // router
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.sendFile("index.html"));
+app.post("/", (req, res) => {
+  let message = req.body.message;
+  console.log(message);
+  res.send(message);
+});
 
 // server listen on localhost:3000
-app.listen(port, () => console.log(`Example app litening on port ${port}!`))
+app.listen(port, () => console.log(`Example app litening on port ${port}!`));
